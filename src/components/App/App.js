2b8ddf6b65ee5a576c1353cf89358febf92aa394
unknown <giachettimilena@gmail.com>
2020-10-07 22:15:15 -0300

    Add get video & captions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,63 @@
-import React, { useEffect} from 'react';
-import logo from '../../assets/images/logo.svg';
+import React, { useEffect, useCallback, useState } from 'react';
 import './App.scss';
 import youtube from '../../apis/youtube';
+import Axios from 'axios';
 
 function App() {
-  useEffect(() => {
-    youtube.get('/search', {
-      params: {
-        q: 'cats'
-      }
-    }).then(response => {
-      console.log(response);
-    })
-  }, [])
+  	const [captions, setCaptions] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 	const getCaptions = useCallback((lang, id) => {
+		Axios.get(`http://video.google.com/timedtext?lang=${lang}&v=${id}`)
+		.then(response => {			
+			console.log(response.data);
+			let parser = new DOMParser();
+			let xmlDoc = parser.parseFromString(response.data,"text/xml");
+			
+			let captionsArray = xmlDoc.getElementsByTagName("text");
+			console.log(captionsArray);
+			let captionsArrayState = [];
+			for (var caption of captionsArray) {
+				// time -> converted to min (todo: improve transformation to ex. 2:32 instead)
+				let time = (caption.getAttribute("start") / 60).toFixed(2) + " min";
+				// inner text
+				let text = caption.childNodes[0];        
+				captionsArrayState.push({id: (time + lang), time: time, text: text});
+			}
+			setCaptions(captionsArrayState);
+		})
+	}, [])
+
+	useEffect(() => {
+		youtube.get('/captions', {
+			params: {
+				videoId: "1gYdJbCzrnY"
+			}
+		}).then(response => {
+			console.log(response.data.items);
+			getCaptions("en", "1gYdJbCzrnY");
+			//response.data.items.forEach(item => getCaptions(item.id));
+		})
+		//getCaptions("en", "1gYdJbCzrnY");
+		//getCaptions("it", "1gYdJbCzrnY");
+
+	}, [getCaptions])
+
+	return (
+		<div className="App">
+		<header className="App-header"></header>
+		<main>
+			{/* todo: improve iframe title */}
+			<iframe id="ytplayer" type="text/html" title="youtube video" width="640" height="360" src="http://www.youtube.com/embed/1gYdJbCzrnY?autoplay=1&origin=http://example.com" frameBorder="0"/>
+			<div className="captions">
+			{captions.length !== 0 ? 
+				captions.map(caption => {
+					return <p key={caption.id}>{caption.time} {caption.text.textContent}</p>
+				})
+			: null}
+			</div>
+		</main>
+		</div>
+	);
 }
 
 export default App;
